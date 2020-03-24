9def0fb40c30c054b6d9902fba6fbabb19bd72b7
sff1019 <starfield1019@gmail.com>
2020-03-24 15:57:46 +0900

    Update

--- app.py
+++ app.py
@@ -16,28 +16,35 @@ app = dash.Dash(__name__, server=server)
 
 
 def exponential_smoothing(beta_1, beta_2, train_loss):
-    ses_loss, des_loss = [], []
-    b = 0
+    ses_list, des_list = [], []
+    b = 0.
 
+    prev_ses_error = train_loss[0]
+    prev_des_error = train_loss[0]
     for epoch in range(len(train_loss)):
         if epoch == 0:
-            ses_loss.append(train_loss[epoch])
-            des_loss.append(train_loss[epoch])
+            ses_list.append(prev_ses_error)
+            des_list.append(prev_des_error)
         elif epoch == 1:
-            b = train_loss[epoch] - des_loss[-1]
-            des_loss.append(
-                (1 - beta_1) * (des_loss[-1] + b) + beta_1 * train_loss[epoch])
-            ses_loss.append(
-                (1 - beta_1) * ses_loss[-1] + beta_1 * train_loss[epoch])
+            b = train_loss[epoch] - prev_des_error
+            des_error = (1 - beta_1) * (prev_des_error + b) + \
+                beta_1 * train_loss[epoch]
+            ses_error = (1 - beta_1) * prev_ses_error + \
+                beta_1 * train_loss[epoch]
+            des_list.append(des_error)
+            ses_list.append(ses_error)
+            prev_ses_error, prev_des_error = ses_error, des_error
         else:
-            des_loss.append(
-                (1 - beta_1) * (des_loss[-1] + b) + beta_1 * train_loss[epoch])
-            b = (1 - beta_2) * b + beta_2 * \
-                (des_loss[-1] - des_loss[-2])
-            ses_loss.append(
-                (1 - beta_1) * ses_loss[-1] + beta_1 * train_loss[epoch])
+            des_error = (1 - beta_1) * (prev_des_error + b) + \
+                beta_1 * train_loss[epoch]
+            ses_error = (1 - beta_1) * prev_ses_error + \
+                beta_1 * train_loss[epoch]
+            des_list.append(des_error)
+            ses_list.append(ses_error)
+            b = (1 - beta_2) * b + beta_2 * (des_error - prev_des_error)
+            prev_ses_error, prev_des_error = ses_error, des_error
 
-    return ses_loss, des_loss
+    return ses_list, des_list
 
 
 if __name__ == '__main__':
@@ -47,16 +54,16 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     epochs = []
-    train_loss, ses_loss, des_loss = [], [], []
-    beta_1 = 0.7
-    beta_2 = 0.6
+    train_loss, ses_error, des_error = [], [], []
+    beta_1 = 0.2
+    beta_2 = 0.3
 
     with jsonlines.open(args.file) as r:
         for obj in r:
-            if 'ses_loss' in obj:
-                epochs.append(obj['epoch'])
-                train_loss.append(obj['train_loss'])
-    ses_loss, des_loss = exponential_smoothing(beta_1, beta_2, train_loss)
+            if 'ses_error' in obj:
+                epochs.append(obj['global_step'])
+                train_loss.append(obj['train_error'])
+    ses_error, des_error = exponential_smoothing(beta_1, beta_2, train_loss)
 
     fig = go.Figure(
         layout=go.Layout(
@@ -67,16 +74,16 @@ if __name__ == '__main__':
     )
     fig.add_trace(go.Scatter(x=epochs, y=train_loss,
                              mode='lines', name='train loss'))
-    fig.add_trace(go.Scatter(x=epochs, y=ses_loss,
+    fig.add_trace(go.Scatter(x=epochs, y=ses_error,
                              mode='lines', name='ses loss'))
-    fig.add_trace(go.Scatter(x=epochs, y=des_loss,
+    fig.add_trace(go.Scatter(x=epochs, y=des_error,
                              mode='lines', name='des loss'))
 
     app.layout = html.Div([
         dcc.Graph(id='plot', figure=fig),
-        dcc.Slider(id='beta_1', min=0, max=1, step=0.01, value=0.5),
+        dcc.Slider(id='beta_1', min=0, max=1, step=0.01, value=0.2),
         html.Div(id='slider_beta_1'),
-        dcc.Slider(id='beta_2', min=0, max=1, step=0.01, value=0.5),
+        dcc.Slider(id='beta_2', min=0, max=1, step=0.01, value=0.3),
         html.Div(id='slider_beta_2'),
     ])
 
@@ -93,7 +100,8 @@ if __name__ == '__main__':
         [Input('beta_1', 'value'), Input('beta_2', 'value')]
     )
     def update_exponential_smoothing(beta_1, beta_2):
-        ses_loss, des_loss = exponential_smoothing(beta_1, beta_2, train_loss)
+        ses_error, des_error = exponential_smoothing(
+            beta_1, beta_2, train_loss)
         fig = go.Figure(
             layout=go.Layout(
                 title='Smoothing Visualizer',
@@ -103,9 +111,9 @@ if __name__ == '__main__':
         )
         fig.add_trace(go.Scatter(x=epochs, y=train_loss,
                                  mode='lines', name='train loss'))
-        fig.add_trace(go.Scatter(x=epochs, y=ses_loss,
+        fig.add_trace(go.Scatter(x=epochs, y=ses_error,
                                  mode='lines', name='ses loss'))
-        fig.add_trace(go.Scatter(x=epochs, y=des_loss,
+        fig.add_trace(go.Scatter(x=epochs, y=des_error,
                                  mode='lines', name='des loss'))
         return fig
 
